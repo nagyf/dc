c04bf0fa0e005710fd19e79c904e1b326d0831a4
Ferenc Nagy <nagy.ferenc.jr@protonmail.com>
2020-03-15 13:01:37 -0400

    Add support for negative numbers.
    '-' cannot be used as a sign because it is the subtraction operation.
    Instead '_' must be used before numbers to indicate a negative number.
    
    Example: '_42' is equal to the number -42

--- src/token.rs
+++ src/token.rs
@@ -20,6 +20,7 @@ pub fn tokenize(str: &str) -> Result<Vec<Op>, String> {
     let length = str.len();
     let buffer = str.to_owned();
     let mut index = 0;
+
     while index < length {
         let ch = buffer.chars().nth(index).unwrap();
         index += 1;
@@ -32,24 +33,26 @@ pub fn tokenize(str: &str) -> Result<Vec<Op>, String> {
             'c' => tokens.push(Op::Clear),
             'd' => tokens.push(Op::Duplicate),
             '+' => tokens.push(Op::Add),
-            '-' => tokens.push(Op::Sub),
             '*' => tokens.push(Op::Mul),
             '/' => tokens.push(Op::Div),
             '%' => tokens.push(Op::Mod),
             '^' => tokens.push(Op::Exp),
-            '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' => {
+            '-' => tokens.push(Op::Sub),
+            '_' | '0'..='9' => {
                 let mut num_str = String::new();
                 num_str.push(ch);
                 while index < length {
                     match buffer.chars().nth(index).unwrap() {
-                        '.' | '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' => {
+                        '.' | '0'..='9' => {
                             num_str.push(buffer.chars().nth(index).unwrap());
                             index += 1;
                         },
                         _ => break,
                     };
                 }
-                tokens.push(Op::Push(num_str.parse::<f64>().unwrap()))
+                num_str = num_str.replace("_", "-");
+                let num = num_str.parse::<f64>().unwrap();
+                tokens.push(Op::Push(num));
             },
             _ => {
                 return Err(format!("Unknown operation: {}", ch))
